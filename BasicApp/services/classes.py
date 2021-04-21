919b77c5c2dde5a2524c81c4ed7603d5333e771e
Kelle Clark <clarkke20@students.ecu.edu>
2021-04-21 19:41:55 -0400

    making basic app into Clik

--- BasicApp/services/classes.py
+++ BasicApp/services/classes.py
@@ -11,5 +11,13 @@ class User(GraphObject):
     created_on = Property()
     last_logon = Property()
 
+class Events(GraphObject):
+    __primarylabel__="event"
+    __primarykey__="email"
+    title=Property()
+    email=Property()
+    detail=Property()
+
+
 
 
